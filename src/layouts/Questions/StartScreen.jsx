4f7405a2dc6c2edce40ebf514f24245c94863e8e
ITSH <itsshahradsheykhlani@gmail.com>
2024-07-01 14:41:55 +0330

    Update StartScreen.jsx

--- src/layouts/Questions/StartScreen.jsx
+++ src/layouts/Questions/StartScreen.jsx
@@ -4,7 +4,7 @@ export default function StartScreen({questions_length, onJournyStart}) {
       <div className="w-8/12 p-2 text-center bg-gray-700 rounded-3xl">
         <span className="text-2xl font-mono font-semibold text-gray-200">🔥 Your {questions_length} question's are ready!</span>
       </div>
-        <span className="font-mono font-semibold text-gray-400 my-6 max-md:text-center">Start your journy by <button onClick={() => onJournyStart()} className="bg-gray-600 px-2 rounded-full text-white">Clicking on me</button></span>
+        <span className="font-mono font-semibold text-gray-400 my-6 max-md:text-center">Start your journy by <button onClick={() => onJournyStart()} className="bg-gray-600 px-2 rounded-full text-white max-md:my-2">Clicking on me</button></span>
     </div>
   </>
 }
